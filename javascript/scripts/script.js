b558fb31e3d26db34caf5ad27d82c6604b13656c
Camila Arce <133790127+cararcel@users.noreply.github.com>
2024-04-29 10:27:31 +0200

    new course git

--- javascript/scripts/script.js
+++ javascript/scripts/script.js
@@ -39,25 +39,58 @@ function afficherEmail(nom, email, score) {
     location.href = mailto
 }
 
+/**
+ * Cette fonction prend un nom en paramètre et valide qu'il est au bon format
+ * ici : deux caractères au minimum
+ * @param {string} nom 
+ * @throws {Error}
+ */
 function validerNom(nom) {
-    if (nom.length >= 2) {
-        return true
+    if (nom.length < 2) {
+        throw new Error("Le nom est ttrop court.")
     }
-    return false
-}
-function validerEmail(email) {
-    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
-    if (emailRegExp.test(email)) {
-        return true
-    }
-    return false
 }
 /**
- * Cette fonction lance le jeu. 
- * Elle demande à l'utilisateur de choisir entre "mots" et "phrases" et lance la boucle de jeu correspondante
+ * Cette fonction prend un email en paramètre et valide qu'il est au bon format. 
+ * @param {string} email 
+ * @throws {Error}
  */
+function validerEmail(email) {
+    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
+    if (!emailRegExp.test(email)) {
+        throw new Error("L'email n'est pas valide.")
+    }
+}
+function afficherMessageErreur(message){
+    let spanErreurMessage = document.getElementById("erreurMessage")
+
+    if(!spanErreurMessage){
+        let popup = document.querySelector(".popup")
+        spanErreurMessage = document.createElement("span")
+        spanErreurMessage.id = "erreurMessage"
+
+        popup.append(spanErreurMessage)
+    }
+    spanErreurMessage.innerText = message 
+}
+function gererFormulaire(scoreEmail) {
+    try {
+        let baliseNom = document.getElementById("nom")
+        let nom = baliseNom.value
+        validerNom(nom)
+
+        let baliseEmail = document.getElementById("email")
+        let email = baliseEmail.value
+        validerEmail(email)
+        afficherMessageErreur("")
+        afficherEmail(nom,email,scoreEmail)
+    } catch(erreur) {
+        afficherMessageErreur(erreur.message)
+    }
+    
+}
+
 function lancerJeu() {
-    // Initialisations
     initAddEventListenerPopup()
     let score = 0
     let i = 0
@@ -68,7 +101,6 @@ function lancerJeu() {
 
     afficherProposition(listeProposition[i])
 
-    // Gestion de l'événement click sur le bouton "valider"
     btnValiderMot.addEventListener("click", () => {
         if (inputEcriture.value === listeProposition[i]) {
             score++
@@ -84,41 +116,23 @@ function lancerJeu() {
         }
     })
 
-    // Gestion de l'événement change sur les boutons radios. 
     let listeBtnRadio = document.querySelectorAll(".optionSource input")
     for (let index = 0; index < listeBtnRadio.length; index++) {
         listeBtnRadio[index].addEventListener("change", (event) => {
-            // Si c'est le premier élément qui a été modifié, alors nous voulons
-            // jouer avec la listeMots. 
             if (event.target.value === "1") {
                 listeProposition = listeMots
             } else {
-                // Sinon nous voulons jouer avec la liste des phrases
                 listeProposition = listePhrases
             }
-            let form = document.querySelector("form")
-            form.addEventListener("submit", (event) => {
-                event.preventDefault()
-
-                let baliseNom = document.getElementById("nom")
-                let nom = baliseNom.value
-
-                let baliseEmail = document.getElementById("email")
-                let email = baliseEmail.value
-
-                if (validerNom(nom) && validerEmail(email)) {
-                    let scoreEmail = `${score} / ${i}`
-                    afficherEmail(nom, email, scoreEmail)
-                } else {
-                    console.log("Error")
-                }
-
-            })
-
-            // Et on modifie l'affichage en direct. 
             afficherProposition(listeProposition[i])
         })
     }
 
+    let form = document.querySelector("form")
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        let scoreEmail = `${score} / ${i}`
+        gererFormulaire(scoreEmail)
+    })
     afficherResultat(score, i)
 }
