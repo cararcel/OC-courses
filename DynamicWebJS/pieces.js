9cad8d85cc4ff5e90a1c88cd344727f7fbb18eff
Camila Arce <133790127+cararcel@users.noreply.github.com>
2024-05-21 18:51:12 +0200

    add pieces and fill json file

---
+++ DynamicWebJS/pieces.js
@@ -0,0 +1,27 @@
+// Get elements from JSON file
+const response = await fetch('pieces-autos.json');
+const pieces = await response.json();
+
+//Create tags
+const article = pieces[0];
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nom;
+const prixElement = document.createElement("p");
+prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie ?? "(aucune catégirie)";
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "pas de description pour le moment.";
+const stockElement = document.createElement("p");
+stockElement.innerText = article.disponibilite ? "En Stock" : "Rupture de stock";
+
+//place tags on DOM
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
+sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(stockElement);
