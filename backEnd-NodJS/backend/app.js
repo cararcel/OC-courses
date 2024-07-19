f7813bfbc6ba734e077b2ba275f37ff1d29ffcb5
Camila Arce <133790127+cararcel@users.noreply.github.com>
2024-07-19 16:00:10 +0200

    Cours:Passez au full stack end

--- backEnd-NodJS/backend/app.js
+++ backEnd-NodJS/backend/app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
 
+const path = require('path');
+
 mongoose
     .connect(
         'mongodb+srv://cararcel:sBl1VU6awVWVYWVl@cluster0.jkrjplp.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0',
@@ -32,5 +34,6 @@ app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
--- backEnd-NodJS/backend/routes/stuff.js
+++ backEnd-NodJS/backend/routes/stuff.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const auth = require('auth');
 const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const stuffCtrl = require('../controllers/stuff');
 
 router.get('/', auth, stuffCtrl.getAllStuff);
-router.post('/', stuffCtrl.createThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
-router.get('/:id', stuffCtrl.getOneThing);
-router.get('/', stuffCtrl.getAllStuff);
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
